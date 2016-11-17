7d4424acaefabe75bd3be94f5e48f0ae7bb68b06
hakurouken <ticshot@gmail.com>
2016-11-18 00:11:36 +0800

    route decorator

---
+++ autoroute/decorator.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+try:
+    from functools import wraps
+except ImportError:
+    def wraps(wrapped, assigned=('__module__', '__name__', '__doc__'),
+              updated=('__dict__',)):
+        def inner(wrapper):
+            for attr in assigned:
+                setattr(wrapper, attr, getattr(wrapped, attr))
+            for attr in updated:
+                getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
+            return wrapper
+        return inner
+
+__all__ = ['route','UrlConf']
+
+UrlConf = namedtuple('UrlConf',['url','name'])
+
+def route(url=None,name=None):
+    def resolver(func):
+        @wraps(func)
+        def wrapper(*args,**kwargs):
+            return func(*args,**kwargs)
+        wrapper._route = UrlConf(url,name)
+        return wrapper
+    return resolver
