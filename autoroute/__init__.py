3d73953af81cd65d397dae94cd30013d1d733004
hakurouken <ticshot@gmail.com>
2016-11-24 00:17:07 +0800

    Add meta info.

--- autoroute/__init__.py
+++ autoroute/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+''' An auto-generated router for django. '''
+__version__ = '0.1.0'
+__author__ = 'HakurouKen'
+__license__ = 'MIT'
 
 from autoroute.decorator import UrlConf,route
 from autoroute.inspector import RouteInspector as Inspector
