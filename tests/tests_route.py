603456e640a62a0e323754f9cb0a3d7b93faa92d
hakurouken <ticshot@gmail.com>
2016-11-28 20:42:13 +0800

    Underscore testcases.

--- tests/tests_route.py
+++ tests/tests_route.py
@@ -39,3 +39,22 @@ class CamelTestCase(SimpleTestCase):
         resolver = resolve('/app/views/custom/')
         self.assertEqual(resolver.func,views.custom_route)
         self.assertEqual(resolver.url_name,'custom-name')
+
+
+@override_settings(ROOT_URLCONF='underscore_urls')
+class UnderscoreTestCase(SimpleTestCase):
+    ''' Test cases for autoroute of `underscore` style. '''
+
+    def test_auto_generated(self):
+        resolver = resolve('/app/views/auto_generated/')
+        self.assertEqual(resolver.func,views.auto_generated)
+
+    def test_with_name(self):
+        resolver = resolve('/app/views/with_name/')
+        self.assertEqual(resolver.func,views.with_name)
+        self.assertEqual(resolver.url_name,'with-name')
+
+    def test_custom(self):
+        resolver = resolve('/app/views/custom/')
+        self.assertEqual(resolver.func,views.custom_route)
+        self.assertEqual(resolver.url_name,'custom-name')
